852b81cc337c7a1c3dc5cce851729615aedf4c38
xivnick <xivnick@gmail.com>
2023-03-20 13:47:03 +0900

    Set the port using environment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
